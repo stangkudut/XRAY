bd7105bd55b4886b64d8816d07fe22893e2f1ae3
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-11 01:39:50 +0700

    Add files via upload

---
+++ menu/menu-backup.sh
@@ -0,0 +1,32 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}----- [ Vmess Menu ] -----${NC}               "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e ""
+echo -e " ${MB}[1]${NC} ${YB}Set Auto Backup${NC} "
+echo -e " ${MB}[2]${NC} ${YB}Backup Data VPS${NC} "
+echo -e " ${MB}[3]${NC} ${YB}Restore Data VPS${NC} "
+echo -e ""
+echo -e " ${MB}[0]${NC} ${YB}Back To Menu${NC}"
+echo -e ""
+echo -e "${BB}———————————————————————————————————————————————————————${NC}"
+echo -e ""
+read -p " Select menu :  "  opt
+echo -e ""
+case $opt in
+1) clear ; autobackup ; exit ;;
+2) clear ; backup ; exit ;;
+3) clear ; restore ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "salah tekan " ; sleep 1 ; vmess ;;
+esac
