a1b9aee08d0e5dadd27a984f6233b3b879860028
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-11 01:05:00 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -30,7 +30,7 @@ cp /etc/shadow backupvps/
 cp /etc/gshadow backupvps/
 cp -r /usr/local/etc/xray backupvps/xray
 cd /root
-zip -r $IP-$date.zip backup > /dev/null 2>&1
+zip -r backupvps.zip backupvps > /dev/null 2>&1
 
 echo -e "
 Detail Backup 
