8c1e4bea30a5b211d2b9483ecd7bc9d897e9118d
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-27 04:25:52 +0700

    Update allxray.sh

--- menu/allxray.sh
+++ menu/allxray.sh
@@ -5,22 +5,21 @@ GB='\e[32;1m'
 YB='\e[33;1m'
 BB='\e[34;1m'
 MB='\e[35;1m'
-CB='\e[35;1m'
+CB='\e[36;1m'
 WB='\e[37;1m'
+BW='\e[30;48;5;15m'
 clear
-echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "             ${WB}----- [ All Xray  Menu ] -----${NC}            "
-echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e ""
-echo -e " ${MB}[1]${NC} ${YB}Create Account All Xray${NC} "
-echo -e " ${MB}[2]${NC} ${YB}Trial Account All Xray${NC} "
-echo -e " ${MB}[3]${NC} ${YB}Extend Account All Xray${NC} "
-echo -e " ${MB}[4]${NC} ${YB}Delete Account All Xray${NC} "
-echo -e " ${MB}[5]${NC} ${YB}Check User Login${NC} "
-echo -e ""
-echo -e " ${MB}[0]${NC} ${YB}Back To Menu${NC}"
-echo -e ""
-echo -e "${BB}———————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║${BW}             ----- [ All Xray  Menu ] -----            ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║${NC}   ${RB}[1]${NC} ${GB}Create Account All Xray${NC}                         ${BB}║${NC}"
+echo -e "${BB}║${NC}   ${RB}[2]${NC} ${GB}Trial Account All Xray${NC}                          ${BB}║${NC}"
+echo -e "${BB}║${NC}   ${RB}[3]${NC} ${GB}Extend Account All Xray${NC}                         ${BB}║${NC}"
+echo -e "${BB}║${NC}   ${RB}[4]${NC} ${GB}Delete Account All Xray${NC}                         ${BB}║${NC}"
+echo -e "${BB}║${NC}   ${RB}[5]${NC} ${GB}Check User Login${NC}                                ${BB}║${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║                   ${RB}[0]${NC} Back To Menu                    ${BB}║${NC}"
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
 echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
