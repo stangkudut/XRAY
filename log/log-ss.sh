3c7c0e23d3ec3ba1fa9cac761eb5441f95433bed
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-29 02:28:57 +0700

    Update log-ss.sh

--- log/log-ss.sh
+++ log/log-ss.sh
@@ -5,31 +5,33 @@ GB='\e[32;1m'
 YB='\e[33;1m'
 BB='\e[34;1m'
 MB='\e[35;1m'
-CB='\e[35;1m'
+CB='\e[36;1m'
 WB='\e[37;1m'
+BW='\e[30;48;5;15m'
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/usr/local/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 clear
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "               ${WB}Log Shadowsocks Account${NC}              "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "  ${YB}You have no existing clients!${NC}"
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║${BW}        ----- [ Log Shadowsocks Account ] -----        ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║             ${RB}You have no existing clients!${NC}             ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo ""
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 log-create
 fi
 clear
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "               ${WB}Log Shadowsocks Account${NC}              "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e " ${YB}User  Expired${NC}  "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║${BW}        ----- [ Log Shadowsocks Account ] -----        ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║${CB}User       ${RB}Expired${NC}                                     ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
 grep -E "^#! " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-echo ""
-echo -e "${YB}tap enter to go back${NC}"
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║                  ${RB}Tap Enter To Go Back                 ${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
 read -rp "Input Username : " user
 if [ -z $user ]; then
 log-create
