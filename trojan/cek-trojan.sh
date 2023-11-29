06dbe503c685f6d5dd7aa8a811d1e81343c5511c
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-30 02:20:38 +0700

    Update cek-trojan.sh

--- trojan/cek-trojan.sh
+++ trojan/cek-trojan.sh
@@ -13,7 +13,7 @@ echo -n >/tmp/other.txt
 data=($(cat /usr/local/etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq))
 echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
 echo -e "${BB}║${BW}          ----- [ Trojan User Login ] -----            ${NC}${BB}║${NC} "
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║═══════════════════════════════════════════════════════║${NC}"
 for akun in "${data[@]}"; do
 if [[ -z "$akun" ]]; then
 akun="Tidak Ada"
@@ -37,7 +37,7 @@ else
 jum2=$(cat /tmp/ipvmess.txt | nl)
 echo "user : $akun"
 echo "$jum2"
-echo -e "${BB}║═══════════════════════════════════════════════════════${NC}"
+echo -e "${BB}║═══════════════════════════════════════════════════════║${NC}"
 fi
 rm -rf /tmp/ipvmess.txt
 done
