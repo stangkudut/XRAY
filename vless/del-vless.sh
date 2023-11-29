7f27551c92196f5759093436c3277f31724eadad
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-30 01:21:14 +0700

    Update del-vless.sh

--- vless/del-vless.sh
+++ vless/del-vless.sh
@@ -5,8 +5,9 @@ GB='\e[32;1m'
 YB='\e[33;1m'
 BB='\e[34;1m'
 MB='\e[35;1m'
-CB='\e[35;1m'
+CB='\e[36;1m'
 WB='\e[37;1m'
+BW='\e[30;48;5;15m'
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#= " "/usr/local/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -41,8 +42,8 @@ clear
 echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
 echo -e "${BB}║${BW}    ----- [ Vless Account has been Deleted ] -----     ${NC}${BB}║${NC} "
 echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
-echo -e " ${YB}Client Name :${NC} $user"
-echo -e " ${YB}Expired On  :${NC} $exp"
+echo -e "${BB}║ ${WB}Client Name :${NC} $user"
+echo -e "${BB}║ ${WB}Expired On  :${NC} $exp"
 echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
 echo -e "${BB}║                  ${RB}Tap Enter To Go Back                 ${BB}║${NC} "
 echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
