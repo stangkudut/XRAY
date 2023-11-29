6cfcef7834da845bc72914f9ba4c8233f7eee2c1
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-30 02:03:20 +0700

    Update ext-xray.sh

--- allxray/ext-xray.sh
+++ allxray/ext-xray.sh
@@ -5,30 +5,32 @@ GB='\e[32;1m'
 YB='\e[33;1m'
 BB='\e[34;1m'
 MB='\e[35;1m'
-CB='\e[35;1m'
+CB='\e[36;1m'
 WB='\e[37;1m'
+BW='\e[30;48;5;15m'
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#&@ " "/usr/local/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "               ${WB}Extend All Xray Account${NC}              "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "  ${YB}You have no existing clients!${NC}"
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║${BW}       ----- [ Extend All Xray Account ] -----         ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║        ${RB}You have no existing clients!${NC}            ${NC}${BB}║${NC} "
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║                  ${RB}Tap Enter To Go Back                 ${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 vmess
 fi
 clear
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "               ${WB}Extend All Xray Account${NC}              "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e " ${YB}User  Expired${NC}  "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║${BW}       ----- [ Extend All Xray Account ] -----         ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║${CB}User       ${RB}Expired${NC}                                     ${NC}${BB}║${NC} "
 grep -E "^#&@ " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-echo ""
-echo -e "${YB}tap enter to go back${NC}"
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║                  ${RB}Tap Enter To Go Back                 ${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
 read -rp "Input Username : " user
 if [ -z $user ]; then
 vmess
@@ -44,12 +46,14 @@ exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 sed -i "/#&@ $user/c\#&@ $user $exp4" /usr/local/etc/xray/config.json
 systemctl restart xray
 clear
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "          ${WB}All Xray Account Success Extended${NC}         "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e " ${YB}Client Name :${NC} $user"
-echo -e " ${YB}Expired On  :${NC} $exp4"
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║${BW}     ----- [ All Xray Account was Extended ] -----     ${NC}${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "${BB}║${WB}Client Name :${NC} $user"
+echo -e "${BB}║${WB}Expired On  :${NC} $exp4"
+echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "${BB}║                  ${RB}Tap Enter To Go Back                 ${BB}║${NC} "
+echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 clear
