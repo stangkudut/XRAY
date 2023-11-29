86bdb0f9761f35155d2affae0be6afce18769f2d
stangkudut <149715132+stangkudut@users.noreply.github.com>
2023-11-30 01:15:09 +0700

    Update try-trojan.sh

--- trojan/try-trojan.sh
+++ trojan/try-trojan.sh
@@ -1,3 +1,14 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+BW='\e[30;48;5;15m'
+clear
 domain=$(cat /usr/local/etc/xray/domain)
 user=trial-`echo $RANDOM | head -c4`
 uuid=$(cat /proc/sys/kernel/random/uuid)
@@ -57,43 +68,35 @@ ISP=$(cat /usr/local/etc/xray/org)
 CITY=$(cat /usr/local/etc/xray/city)
 systemctl restart xray
 clear
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${BW}          ----- [ Try Trojan Account ] -----           ${NC}${BB}║${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Remarks       : ${user}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} ISP           : $ISP" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} City          : $CITY" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Host/IP       : ${domain}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Wildcard      : (bug.com).${domain}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Port TLS      : 443" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Port NTLS     : 80" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Port gRPC     : 443" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Alt Port TLS  : 2053, 2083, 2087, 2096, 8443" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Alt Port NTLS : 8080, 8880, 2052, 2082, 2086, 2095" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Password      : ${uuid}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Network       : Websocket, gRPC" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Path          : /trojan" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} ServiceName   : trojan-grpc" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}║${NC} Alpn          : h2, http/1.1" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "                Trial Trojan Account                " | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Remarks       : ${user}" | tee -a /user/log-trojan-$user.txt
+echo -e "ISP           : $ISP" | tee -a /user/log-trojan-$user.txt
+echo -e "City          : $CITY" | tee -a /user/log-trojan-$user.txt
+echo -e "Host/IP       : ${domain}" | tee -a /user/log-trojan-$user.txt
+echo -e "Wildcard      : (bug.com).${domain}" | tee -a /user/log-trojan-$user.txt
+echo -e "Port TLS      : 443" | tee -a /user/log-trojan-$user.txt
+echo -e "Port NTLS     : 80" | tee -a /user/log-trojan-$user.txt
+echo -e "Port gRPC     : 443" | tee -a /user/log-trojan-$user.txt
+echo -e "Alt Port TLS  : 2053, 2083, 2087, 2096, 8443" | tee -a /user/log-trojan-$user.txt
+echo -e "Alt Port NTLS : 8080, 8880, 2052, 2082, 2086, 2095" | tee -a /user/log-trojan-$user.txt
+echo -e "Password      : ${uuid}" | tee -a /user/log-trojan-$user.txt
+echo -e "Network       : Websocket, gRPC" | tee -a /user/log-trojan-$user.txt
+echo -e "Path          : /trojan" | tee -a /user/log-trojan-$user.txt
+echo -e "ServiceName   : trojan-grpc" | tee -a /user/log-trojan-$user.txt
+echo -e "Alpn          : h2, http/1.1" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
 echo -e "Link TLS      : ${trojanlink1}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
 echo -e "Link NTLS     : ${trojanlink2}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
 echo -e "Link gRPC     : ${trojanlink3}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
 echo -e "Format Clash  : http://$domain:8000/trojan/trojan-$user.txt" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
 echo -e "Expired On    : $exp" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}" | tee -a /user/log-trojan-$user.txt
-echo -e "${BB}╔═══════════════════════════════════════════════════════╗${NC}"
-echo -e "${BB}║                  ${RB}Tap Enter To Go Back                 ${BB}║${NC} "
-echo -e "${BB}╚═══════════════════════════════════════════════════════╝${NC}"
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
 echo " " | tee -a /user/log-trojan-$user.txt
 echo " " | tee -a /user/log-trojan-$user.txt
 echo " " | tee -a /user/log-trojan-$user.txt
